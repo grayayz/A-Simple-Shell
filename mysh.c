9f7406ca779d45a0db1730078dec05583ae28dc1
SamiyahS <samiyahsyed@gmail.com>
2026-04-12 17:21:23 -0400

    fixed wildcard i think

--- mysh.c
+++ mysh.c
@@ -22,15 +22,26 @@ typedef struct command{
 
 } Command;
 
+typedef struct pipeCommand{
+    int in_fd;
+    int out_fd;
+    char** argList;
+    char* commandType;
+    int commandTypeNum;
+} pipeCommand;
+
 void commandParsing (char* commandString, int inputType);
     //inputType options: 0 = batch, 1 = no file interactive
 
-void execution (struct command* commandPtr);
+void execution (void *);
     //actual execution of command, only run after commandParsing
     //make sure to print the exit status and if it worked
     //make sure to free(command) at end;
 
-void wildcard (char* tempStr, char** argList, int* argNum, int* argListCap);
+void parsePipe(char* commandStr);
+
+
+void wildcard (char* tempStr, char*** argList, int* argNum, int* argListCap);
 
 
 int main(int argc, char **argv)
@@ -324,9 +335,11 @@ void commandParsing (char* commandString, int inputType){
                 continue;
             }
             else if (commandString[i] == '|'){
+
+                 // printf("did we get here? \n");
                 pipe = 1;
-                //might have to be done in the execution method? 
-                //CODE 
+                parsePipe(commandString);
+                return;
                 continue;
             }
 
@@ -342,7 +355,7 @@ void commandParsing (char* commandString, int inputType){
                     tempStr[j + tempIndex] = commandString[i+j];
                 }
 
-                wildcard(tempStr, argList, &argNum, &argListCap);
+                wildcard(tempStr, &argList, &argNum, &argListCap);
 
                 i = i + j;
 
@@ -402,21 +415,191 @@ void commandParsing (char* commandString, int inputType){
 
 }
 
-void wildcard (char* tempStr, char **argList, int* argNum, int* argListCap){
+void parsePipe(char* commandStr){
+
+    //printf("pipe parsing \n");
+    //we know we have n pipes that will connect n+1 processes
+        //uses pipe() and dup2() to connect input/output
+    int arrLen = 10;
+    int commandNum = 0;
+    struct pipeCommand** commandArr = (struct pipeCommand**)malloc(sizeof(pipeCommand*) * arrLen);
+    struct pipeCommand* commandPtr = (struct pipeCommand*)malloc(sizeof(pipeCommand));
+    commandArr[0] = commandPtr;
+    commandNum ++;
+
+    int i = 0;
+    int j = 0;
+        //while j = 0 input, while j = 1 output
+
+    //add every word to argList until you reach the pipe
+        //once you finish the first word it is the command
+            //commandPtr -> command = argList[0];
+            //create a new char arr for command, then memcpy to get the command
+        //once argNum = 1, you are just propogating the argList 
+            //which is a single char[] 
+            //once you've hit the pipe and strlen(argList) > 0
+                //commandPtr -> arglist = argList;
+    int type = -1;
+    int argCap = 100;
+    char* argListPtr = (char*)malloc(sizeof(char) * argCap);   
+
+    while (i < strlen(commandStr)){
+        argCap = 100;
+        int argLen = 0;
+        int argNum = 0;
+        char* argListPtr = (char*)malloc(sizeof(char) * argCap);   
+       
+        if(commandStr[i] == '/'){
+            //this is a path to an executable 
+            type = 0;
+        }
+
+        if(commandStr[i] == '|'){
+            if(j == 0){j = 1;}
+            else if (j == 1){//we are working on a new command in the commandArr
+                j = 0;
+                struct pipeCommand* commandPtr = (struct pipeCommand*)malloc(sizeof(pipeCommand));
+                if (commandNum < arrLen){
+                    commandArr = realloc(commandArr, arrLen * 2);
+                    arrLen = arrLen * 2;
+                }
+
+                commandArr[commandNum] = commandPtr;
+                commandNum++;
+            }
+            if(strlen(argListPtr) > 0){
+                commandPtr->argList = &argListPtr;
+            }
+
+            free (argListPtr);
+            char* argListPtr = (char*)malloc(sizeof(char) * argCap);   
+
+        }
+        if(commandStr[i] == ' '){
+            if(argNum == 0){
+                char* command = (char*)malloc(sizeof(char)*strlen(argListPtr));
+                memcpy(command, argListPtr, strlen(argListPtr));
+                commandPtr->commandType = command;
+                memset(argListPtr, 0, strlen(argListPtr));
+                argNum ++;
+            }
+            else{
+                continue;
+            }
+        }
+
+        if(argLen == argCap){
+            argListPtr = realloc(argListPtr, argCap * 2);
+            argCap = argCap * 2;
+        }
+
+        argListPtr[argLen] = commandStr[i];
+        argLen ++;
+
+        i++;
+    }
+    //parse the command to isolate smaller command blocks such that 
+        //command arg | command arg --> each one of these is a "process"
+        //can have no args or multiple args 
+        //propogate a struct that sequentially stores these processes so that we can execute
+
+    //we have propogated the struct, we need to figure out what kind of command it is now 
+    if(type == -1){
+        //not an executable 
+        //check if builtin command: cd, pwd, which, exit 
+        if((strcmp(commandPtr->commandType, "cd") == 0) || (strcmp(commandPtr->commandType, "pwd") == 0) || (strcmp(commandPtr->commandType, "which") == 0) || (strcmp(commandPtr->commandType, "exit") == 0)){
+            type = 1;
+        }
+        else{type = 2;}
+    }
+
+    commandPtr->commandTypeNum = type;
+
+    //after we have parsed and propogated, call the execute function
+    //free each commandPtr after calling the execute function 
+    i = 0;
+    int nextInput = STDIN_FILENO;
+    
+    while (i < commandNum-1){
+        int fd[2];
+        pipe(fd);
+
+        commandArr[i] -> in_fd = nextInput;
+        commandArr[i] -> out_fd = fd[1];
+
+        nextInput = fd[0];
+        i++;
+    }
+
+    commandArr[commandNum-1] -> in_fd = nextInput;
+    commandArr[commandNum-1] -> out_fd = STDOUT_FILENO;
+
+    //okay now we have to call the execute function 
+    i = 0;
+
+    while (i < commandNum){
+        pid_t pid = fork();
+
+        if (pid == 0){
+            //this is a child command
+            execution(commandArr[i]);
+        }
+        else{
+            //this is a parent 
+            if (commandArr[i]->in_fd != STDIN_FILENO) {
+                    close(commandArr[i]->in_fd);
+                }
+                if (commandArr[i]->out_fd != STDOUT_FILENO) {
+                    close(commandArr[i]->out_fd);
+                }
+        }
+
+        i++;
+    }
+
+
+     for (int i = 0; i < commandNum; i++) {
+            wait(NULL);
+    }
+
+    //now that we've called all the commands we have to go through and free everything
+    for (int i = 0; i < commandNum; i++){
+        //commandArr has commandNum commandPtr's that have up to one argListPtr
+        if(commandArr[i]->argList != NULL){
+            free(commandArr[i]->argList);
+        }
+        free(commandArr[i]);
+    }
+    //free everything 
+    free(commandArr);
+    return;
+}
+
+void execution (void * ptr){
+    
+    
+    
+    return;
+}
+
+
+void wildcard (char* tempStr, char ***argList, int* argNum, int* argListCap){
     //searches for all files in the dir that matches the wildcard format and adds to argList
     //makes sure to update argNum and argListCap as needed 
 
     //open the directory we're in 
-    char* prefix = (char*)malloc (sizeof(char) * 100);
-    char* suffix = (char*)malloc (sizeof(char) * 100);
+    char* prefix = (char*)malloc (sizeof(char) * (strlen(tempStr) + 1));
+    char* suffix = (char*)malloc (sizeof(char) * (strlen(tempStr) + 1));
     
     int i = 0;
     int ogArgNum = *argNum;
 
-    while(i != '*'){
+    while(tempStr[i] != '*'){
         prefix[i] = tempStr[i];
         i++;
     }
+
+    prefix[i] = '\0';
 
     int j = i + 1;
     i = 0;
@@ -426,6 +609,8 @@ void wildcard (char* tempStr, char **argList, int* argNum, int* argListCap){
         i++;
         j++;
     }
+
+    suffix[i] = '\0';
 
     //now open directory and search through all files to see if they contain the prefix and suffix
         //ensure that int prefixStart < int suffixStart
@@ -440,18 +625,19 @@ void wildcard (char* tempStr, char **argList, int* argNum, int* argListCap){
           if(strcmp(de->d_name, ".") == 0 || strcmp(de->d_name, "..") == 0){
                 continue;
             }
-        char* name = (char*)malloc (sizeof(char) * 100);
+        char* name = (char*)malloc (sizeof(char) * strlen(de->d_name) + 1);
+        strcpy(name, de->d_name);
         
 
         if((strstr(name, prefix) != NULL) && ((strstr(name, suffix)) != NULL)){
             if(strstr(name, suffix) - (strstr(name, prefix)) > 0){
                 //add the name to arglist 
                  if (*argNum == *argListCap){
-                    argList = realloc (argList, *argListCap * 2);
+                    *argList = realloc (*argList, sizeof(char*) * *argListCap * 2);
                     *argListCap = *argListCap * 2;
                 }
 
-                argList[*argNum] = name;
+                (*argList)[*argNum] = name;
                 (*argNum)++;
             }
             else{free(name);}
@@ -465,13 +651,15 @@ void wildcard (char* tempStr, char **argList, int* argNum, int* argListCap){
     if(ogArgNum == *argNum){
         //didn't find anything that fit pattern
         if (*argNum == *argListCap){
-                    argList = realloc (argList, *argListCap * 2);
+                    *argList = realloc (*argList, sizeof(char*) * *argListCap * 2);
                     *argListCap = *argListCap * 2;
                 }
 
-                argList[*argNum] = tempStr;
+                (*argList)[*argNum] = tempStr;
             (*argNum)++;
     }
+
+    closedir(dir);
 
     return;
 
